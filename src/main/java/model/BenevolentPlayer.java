55d8db229641b83b380affc8aa38d0034c231d5d
Kaur-jasmine-14 <jasmine14concordia@gmail.com>
2019-11-23 13:01:24 -0500

    benevolent bug fixes

--- src/main/java/model/BenevolentPlayer.java
+++ src/main/java/model/BenevolentPlayer.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 public class BenevolentPlayer extends Player {
 
     BenevolentPlayer benevolentPlayer;
+    GameActions gameActions;
     Player player;
     String weakestCountry;
 
@@ -20,20 +21,23 @@ public class BenevolentPlayer extends Player {
 
     @Override
     public boolean reinforce(GameData game, String country, int num) {
-        setOwnedArmies(player.getOwnedArmies());
-        game.setActivePlayer(player);
-        weakestCountry = getWeakestCountry(player).getCountryName();
+        boolean checkForReinforce = gameActions.assignReinforcementArmies(game, player);
+        boolean checkforCardExchange = player.cardExchange(game,null);
+        if (checkForReinforce && checkforCardExchange) {
+            game.setActivePlayer(player);
+            weakestCountry = getWeakestCountry(player).getCountryName();
 
-        if (player.getOwnedArmies() > 0) {
-            Country c = player.getOwnedCountries().get(weakestCountry.toLowerCase());
-            int existingArmies = c.getNumberOfArmies();
-            existingArmies += player.getOwnedArmies();
-            c.setNumberOfArmies(existingArmies);
-            player.setOwnedArmies(player.getOwnedArmies());
-            notifyObservers(Integer.toString(player.getOwnedArmies()) + " armies reinforced at " + weakestCountry + ". Remaining reinforcement armies: " + Integer.toString(player.getOwnedArmies()) + "\n");
-            game.setGamePhase(Phase.ATTACK);
-        } else {
-            notifyObservers(player.getPlayerName() + " doesn't have " + player.getOwnedArmies() + " armies to reinforce. Invalid command.");
+            if (player.getOwnedArmies() > 0) {
+                Country c = player.getOwnedCountries().get(weakestCountry.toLowerCase());
+                int existingArmies = c.getNumberOfArmies();
+                existingArmies += player.getOwnedArmies();
+                c.setNumberOfArmies(existingArmies);
+                player.setOwnedArmies(player.getOwnedArmies());
+                notifyObservers(Integer.toString(player.getOwnedArmies()) + " armies reinforced at " + weakestCountry + ". Remaining reinforcement armies: " + Integer.toString(player.getOwnedArmies()) + "\n");
+                game.setGamePhase(Phase.ATTACK);
+            } else {
+                notifyObservers(player.getPlayerName() + " doesn't have " + player.getOwnedArmies() + " armies to reinforce. Invalid command.");
+            }
         }
     }
 
@@ -44,11 +48,55 @@ public class BenevolentPlayer extends Player {
     }
 
     @Override
-    public FortificationCheck fortify(GameData game, String fromCountry, String toCountry, int num){
+    public FortificationCheck fortify(GameData game, String fromCountry, String toCountry, int num) {
 
     }
 
-    public Country getWeakestCountry(Player player){
+    public void fortifyData(Player player) {
+
+        Collection<Country> countries = player.getOwnedCountries().values();
+        Country[] targetCountries = countries.toArray(new Country[0]);
+
+
+        int length = targetCountries.length;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                if (targetCountries[j].getNumberOfArmies() > targetCountries[j + 1].getNumberOfArmies()) {
+                    // swap arr[j+1] and arr[i]
+                    Country temp = targetCountries[j];
+                    targetCountries[j] = targetCountries[j + 1];
+                    targetCountries[j + 1] = temp;
+                }
+            }
+        }
+
+        Country[] sourceCountries = countries.toArray(new Country[0]);
+        int lengthSources = sourceCountries.length;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                if (sourceCountries[j].getNumberOfArmies() > sourceCountries[j + 1].getNumberOfArmies()) {
+                    // swap arr[j+1] and arr[i]
+                    Country temp = sourceCountries[j];
+                    sourceCountries[j] = sourceCountries[j + 1];
+                    sourceCountries[j + 1] = temp;
+                }
+            }
+        }
+
+        MapValidation mv = new MapValidation();
+        boolean check;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < lengthSources; j++) {
+                check = mv.fortificationConnectivityCheck(player, sourceCountries[j].getCountryName(), targetCountries[i].getCountryName());
+                if (check) {
+
+                }
+            }
+        }
+
+    }
+
+    public Country getWeakestCountry(Player player) {
         Collection<Country> countries = player.getOwnedCountries().values();
         Country weakest_country = null;
         int min_army = 500;
