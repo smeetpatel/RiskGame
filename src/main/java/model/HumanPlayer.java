dccf496ab702aa8007f399965f3ae09649aec2fd
Starc510 <tirthshah510@gmail.com>
2019-11-23 20:48:06 -0500

    javadoc added to the human player

--- src/main/java/model/HumanPlayer.java
+++ src/main/java/model/HumanPlayer.java
@@ -17,11 +17,11 @@ public class HumanPlayer extends Player {
     }
 
     /**
-     * {@inheritDoc}
+     * Reinforcement phase for human player
      * @param game Represents the state of the game
      * @param countryName Reinforce armies here
      * @param num Reinforce this many armies
-     * @return
+     * @return true if reinforcement is successful else false
      */
     public boolean reinforce(GameData game, String countryName, int num){
         game.setActivePlayer(this);
@@ -57,14 +57,14 @@ public class HumanPlayer extends Player {
     }
 
     /**
-     * {@inheritDoc}
+     * Attack phase for human player
      * @param game Represents the state of the game.
      * @param countryFrom Country doing the attack
      * @param countryTo Country that is defending
      * @param numberOfDice Number of dice attacker wishes to roll
      * @param defendDice Number of dice defender wishes to roll
      * @param defendingPlayer Player owning the defending country
-     * @return
+     * @return true if attack is successful else false
      */
     public boolean attack(GameData game, String countryFrom, String countryTo, int numberOfDice, int defendDice, Player defendingPlayer){
         Country attackingCountry = game.getMap().getCountries().get(countryFrom.toLowerCase());
@@ -126,12 +126,12 @@ public class HumanPlayer extends Player {
     }
 
     /**
-     * {@inheritDoc}
+     * Fortification phase for the human player
      * @param game represents state of the game
      * @param fromCountry country from armies send
      * @param toCountry country to armies placed
      * @param num total number of armies to send from one country to another country
-     * @return
+     * @return status of the fortification phase
      */
     public FortificationCheck fortify(GameData game, String fromCountry, String toCountry, int num){
         MapValidation mv = new MapValidation();
