4d3d17058beecd60bd6c244ffe9f7d6b97ee2b2b
Starc510 <tirthshah510@gmail.com>
2019-11-23 20:56:19 -0500

    javadoc added for cheater player

--- src/main/java/model/CheaterPlayer.java
+++ src/main/java/model/CheaterPlayer.java
@@ -17,11 +17,11 @@ public class CheaterPlayer extends Player{
     }
 
     /**
-     * {@inheritDoc}
+     * Reinforcement phase for cheater player
      * @param game Represents the state of the game
      * @param countryName Reinforce armies here
      * @param num Reinforce this many armies
-     * @return
+     * @return true if reinforcement is successful
      */
     public boolean reinforce(GameData game, String countryName, int num){
 
@@ -34,14 +34,14 @@ public class CheaterPlayer extends Player{
     }
 
     /**
-     * {@inheritDoc}
+     * Attack phase for cheater player
      * @param game Represents the state of the game.
      * @param countryFrom Country doing the attack
      * @param countryTo Country that is defending
      * @param numberOfDice Number of dice attacker wishes to roll
      * @param defendDice Number of dice defender wishes to roll
      * @param defendingPlayer Player owning the defending country
-     * @return
+     * @return true if attack is successful else false
      */
     public boolean attack(GameData game, String countryFrom, String countryTo, int numberOfDice, int defendDice, Player defendingPlayer){
 
@@ -60,12 +60,12 @@ public class CheaterPlayer extends Player{
     }
 
     /**
-     * {@inheritDoc}
+     * Fortification phase for cheater player
      * @param game represents state of the game
      * @param fromCountry country from armies send
      * @param toCountry country to armies placed
      * @param num total number of armies to send from one country to another country
-     * @return
+     * @return status of the fortification phase
      */
     public FortificationCheck fortify(GameData game, String fromCountry, String toCountry, int num){
 
